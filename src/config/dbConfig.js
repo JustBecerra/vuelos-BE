ddba6df2dc129c9b040b792f1c860c7e1aba4c6a
JustBecerra <justj.becerra@gmail.com>
2024-10-03 15:30:43 -0300

    delete comments

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,11 +2,10 @@ const { Sequelize } = require("sequelize")
 const sequelize = new Sequelize(
 	"postgres://postgres:54321@localhost:5432/vuelosDB",
 	{
-		logging: false, // Disable logging
+		logging: false,
 	}
 )
 
-// Import models
 const Users = require("../models/User")(sequelize)
 const Flights = require("../models/Flight")(sequelize)
 const Airships = require("../models/Airship")(sequelize)
@@ -14,7 +13,6 @@ const Connections = require("../models/Connection")(sequelize)
 const Clients = require("../models/Client")(sequelize)
 const Images = require("../models/Image")(sequelize)
 
-// Assign models to sequelize instance
 sequelize.models = {
 	Users,
 	Flights,
@@ -24,7 +22,6 @@ sequelize.models = {
 	Images,
 }
 
-// Define associations
 Users.hasMany(Flights, { foreignKey: "createdBy", as: "flights" })
 Flights.hasMany(Connections, { foreignKey: "flight_id", as: "connections" })
 Airships.hasMany(Flights, { foreignKey: "airship_id", as: "flights" })
@@ -32,7 +29,6 @@ Airships.hasMany(Images, { foreignKey: "airship_id", as: "images" })
 Clients.belongsToMany(Flights, { through: "flight_client" })
 Flights.belongsToMany(Clients, { through: "flight_client" })
 
-// Sync models
 sequelize.sync()
 module.exports = {
 	...sequelize.models,
