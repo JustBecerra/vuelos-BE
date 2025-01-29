35e808c79c8ce4f3a910d421ca801ed043efd15c
JustBecerra <justj.becerra@gmail.com>
2025-01-29 14:57:31 -0300

    add origin wildcard

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors"
 
 const corsConfig = cors({
-	origin: ["https://tango-jets-fe.vercel.app", "http://localhost:4321"],
+	origin: "*",
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	allowedHeaders: ["Content-Type", "Authorization"],
 	credentials: true,
